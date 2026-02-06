2cd916471e7577393920835f8cc90f17f3afdd67
arthurfranca <arthur.a.franca@gmail.com>
2026-02-06 19:03:25 -0300

    nappify

--- src/index.tsx
+++ src/index.tsx
@@ -89,7 +89,21 @@ async function doInit() {
       console.error(e);
     }
   }
-  const login = Login.snapshot();
+
+  try {
+    // @ts-ignore
+    const nostr = window.nostr;
+    if (nostr?.peekPublicKey && !Login.takeSnapshot()) {
+      const pk = await nostr.peekPublicKey();
+      if (pk) {
+        Login.loginWithPubkey(pk);
+      }
+    }
+  } catch (e) {
+    console.debug(e);
+  }
+
+  const login = Login.takeSnapshot();
   const follows = login?.state?.follows;
   await System.Init(follows);
   syncClock();
